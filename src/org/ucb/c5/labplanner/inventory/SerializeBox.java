81708b0adfc2ef22c1eb6044c482d44df01daf84
Michael Danielian <45112838+MolecularElk@users.noreply.github.com>
2022-11-15 12:50:46 -0800

    Update SerializeBox.java
    This is the final working version of SerializeBox

--- src/org/ucb/c5/labplanner/inventory/SerializeBox.java
+++ src/org/ucb/c5/labplanner/inventory/SerializeBox.java
@@ -23,6 +23,7 @@ public class SerializeBox {
      * @throws Exception
      */
     public void run(Box abox, String path) throws Exception {
+        //the labels on the top of the file
         StringBuilder sb = new StringBuilder();
         sb.append(">name").append('\t').append(abox.getName()).append("\n");
         sb.append(">description").append('\t').append(abox.getDescription()).append("\n");
@@ -51,6 +52,9 @@ public class SerializeBox {
     }
 
     /**
+     * This will output in a code-readable format, which can
+     * be input into ParseBox
+     *
      * @param abox The Box to be serialized
      * @param path The path and filename for the destination file
      * @throws Exception
@@ -65,21 +69,39 @@ public class SerializeBox {
 
         //test all six possible arrays that can be input
         for (int i = 0; i < 6; i++) {
+            //if there are none to be read, will reset string back to this checkpoint
             int sbCheckpoint = sb.length();
+
+            //used to help format if none of the inputs are real values
             boolean flag = false;
+
+            //creates the header of each section
             sb.append(inputLabelText(i) + "\t1\t2\t3\t4\t5\t6\t7\t8\t9"+"\n");
 
+            //Sample input, from the box input
             Sample[][] input = abox.getSamples();
+
+            //loops through the box and examines each value
             for (int j = 0; j < input.length; j++) {
+                //char of which row
                 sb.append((char)(65 + j)).append("\t");
+
+                //making sure each value is valid
                 for (int k = 0; k < input[0].length; k++) {
+
+                    //making sure a value exists
                     if (input[j][k] != null || !input[j][k].equals("")) {
+                        //changes flag to true, meaning to output values
                         flag = true;
+
+                        //appending new value to string reader
                         sb.append(SampleFunction(i, input[j][k])).append("\t");
                     }
                 }
+                //for formatting
                 sb.append("\n");
             }
+            //for formatting
             sb.append("\n");
 
             //this means there is no needed text output
@@ -88,9 +110,11 @@ public class SerializeBox {
             }
         }
 
+        //outputs to the desired file location
         FileUtils.writeFile(sb.toString(), path);
     }
 
+    //used to help format which text will appear above each section
     private String inputLabelText(int a) {
         switch(a) {
             case 0:
@@ -109,7 +133,9 @@ public class SerializeBox {
         return "";
     }
 
+    //used to help decide which part of samples should be output
     private String SampleFunction(int i, Sample a) {
+        //switch case being which array its inputting to
         switch(i) {
             case 0:
                 if (a.getLabel() == null|| a.getLabel().equals(""))
@@ -120,9 +146,19 @@ public class SerializeBox {
                     return "";
                 return a.getSidelabel();
             case 2:
+                //due to the enum, extra formatting is required
                 if (a.getConcentration() == null || a.getConcentration().equals(""))
                     return "";
-                return a.getConcentration().toString();
+
+                //chaning the string into something that can be read in ParseBox
+                String conc = a.getConcentration().toString();
+                if (conc.charAt(0) == 'u') {
+                    conc = conc.substring(2) + conc.substring(0, 2);
+                }
+                if (conc.charAt(0) == '2') {
+                    conc = conc.substring(0, 1) + '.' + conc.substring(1);
+                }
+                return conc;
             case 3:
                 if (a.getConstruct() == null || a.getConstruct().equals(""))
                     return "";
